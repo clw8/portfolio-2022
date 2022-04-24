b03773db4f162e241f623ec76d2108e44aa8ae3f
chris <chris.w894j@gmail.com>
2022-04-24 14:41:50 +0100

    header test fix

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -4,17 +4,25 @@ import {fireEvent, render } from '@testing-library/react';
 import Header from "../header.js"
 
 describe("Header", () => {
+  let header;
+  const onMouseEnter = () => {}
+  const onMouseOut = () => {}
+
+  beforeAll(() => {
+    header = render(<Header {...{onMouseEnter, onMouseOut, show: true}} />)
+  })
+
   it("renders correctly", () => {
-    const onMouseEnter = () => {}
-    const onMouseOut = () => {}
-    const show = true;
-
-    const {getByRole, asFragment} = render(<Header {...{onMouseEnter, onMouseOut, show}} />)
-    const firstRender = asFragment()
-
-    const projectsLink = getByRole("link", {name: "Projects"});
-    fireEvent.mouseEnter(projectsLink);
-
+    const firstRender = header.asFragment()
+    
     expect(firstRender).toMatchSnapshot()
+  })
+  
+  it("renders hover effects correctly", async() => {
+    setTimeout(async() => {
+      const projectsLink = await header.findByRole("link", {name: "Projects"});
+      fireEvent.mouseEnter(projectsLink);
+      expect(onMouseEnter).toHaveBeenCalledTimes(1)
+    }, 1000)
   })
 })
